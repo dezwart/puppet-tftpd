5b620da9265ca9764ecabb3706316d739ddb65be
Pete de Zwart <dezwart@gmail.com>
2012-12-27 23:12:37 +1100

    Linted.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,22 @@
 # Parameters are extraneous for the moment, e.g. /etc/inetd.conf is not
 # changed.
 class tftpd( $tftpd_user = 'nobody',
-    $tftpd_group = 'nogroup',
-    $tftp_dir = '/srv/tftp' ) {
+  $tftpd_group = 'nogroup',
+  $tftp_dir = '/srv/tftp' ) {
 
-    $package = 'tftpd'
+  $package = 'tftpd'
 
-    package { $package:
-        ensure  => installed,
-    }
+  package { $package:
+    ensure  => installed,
+  }
 
-    file { $tftp_dir:
-        ensure  => directory,
-        owner   => $tftpd_user,
-        group   => $tftpd_group,
-        mode    => '0550',
-        require => Package[$package],
-    }
+  file { $tftp_dir:
+    ensure  => directory,
+    owner   => $tftpd_user,
+    group   => $tftpd_group,
+    mode    => '0550',
+    require => Package[$package],
+  }
 }
+
+/* vim: set ts=2 sw=2 sts=2 tw=0 et:*/
